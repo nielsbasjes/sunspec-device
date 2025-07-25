7491afb63c557a2048df7847fa9645215bd19515
Niels Basjes <niels@basjes.nl>
2025-07-25 13:54:44 +0200

    docs: Cleanup SunSpec Home Assistant demo script

--- demo/SunSpec_to_MQTT_to_HomeAssistant.main.kts
+++ demo/SunSpec_to_MQTT_to_HomeAssistant.main.kts
@@ -57,7 +57,7 @@ import java.time.Instant
 import kotlin.system.exitProcess
 import kotlin.time.Duration.Companion.hours
 
-val modbusHost           :String = "sunspec.iot.basjes.nl"
+val modbusHost           :String? = null // "sunspec.iot.basjes.nl"
 val modbusPort           :Int    = 502 // The default MODBUS TCP port
 val modbusUnit           :Int    = 126 // SMA uses 126, other vendors can differ
 
@@ -65,68 +65,38 @@ val mqttBrokerHost      :String? = null //"localhost"
 val mqttBrokerPort      :Int     = 1883
 val mqttTopic           :String? = "energy/solar"
 
-//val mqttUser            :String? = null TODO: If you need it you must change the code a bit.
-//val mqttPassword        :String? = null TODO: If you need it you must change the code a bit.
-
 // This is useful if you want to set a different hostname (shown in the HA Gauge label and such)
 val homeAssistantDeviceName: String? = null
 
-fun allTheFieldsIWant(device: SchemaDevice): List<Field> {
+/** @return the relation between each field and the used name at the Json side */
+fun allTheFieldsIWant(device: SchemaDevice): MutableList<Pair<String, Field>> {
     // Use these fields as Measurements
-    val allFields = mutableListOf<Field>()
+    val allFields = mutableListOf<Pair<String, Field>>()
 
-    // Brute force want all fields
-//    device
-//        .fields
-//        .filter { !it.isSystem }
-//        .forEach { allFields.add(it) }
+    // From Model 1 the "Manufacturer", "Model", "Version" and "Serial Number" must ALWAYS be added
+    allFields.add("Model_1_Manufacturer"    to device.wantField("Model 1",   "Manufacturer"              ))
+    allFields.add("Model_1_Model"           to device.wantField("Model 1",   "Model"                     ))
+    allFields.add("Model_1_Version"         to device.wantField("Model 1",   "Version"                   ))
+    allFields.add("Model_1_Serial_Number"   to device.wantField("Model 1",   "Serial Number"             ))
 
+    // You now need to add all fields you want below here
+    // Example:
+    //    This links the json field name going to MQTT to the Modbus Schema field from the device
+    //    allFields.add("Model_101_AC_Current" to device.wantField("Model 101", "AC Current" ))
+    // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    // FIXME: Add all fields you need here (this script will produce all possible output if this is empty).
+    //        You should really only include the fields you want to avoid performance problems.
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-    // Or a smarter more efficient selection
-
-    // These are always needed
-    allFields.add(device.wantField("Model 1",   "Manufacturer"              ))
-    allFields.add(device.wantField("Model 1",   "Model"                     ))
-    allFields.add(device.wantField("Model 1",   "Version"                   ))
-    allFields.add(device.wantField("Model 1",   "Serial Number"             ))
-
-    allFields.add(device.wantField("Model 101", "AC Current"                ))
-    allFields.add(device.wantField("Model 101", "AC Current Phase A"        ))
-    allFields.add(device.wantField("Model 101", "AC Current Phase B"        ))
-    allFields.add(device.wantField("Model 101", "AC Current Phase C"        ))
-    allFields.add(device.wantField("Model 101", "AC Voltage Phase AB"       ))
-    allFields.add(device.wantField("Model 101", "AC Voltage Phase BC"       ))
-    allFields.add(device.wantField("Model 101", "AC Voltage Phase CA"       ))
-    allFields.add(device.wantField("Model 101", "AC Voltage Phase AN"       ))
-    allFields.add(device.wantField("Model 101", "AC Voltage Phase BN"       ))
-    allFields.add(device.wantField("Model 101", "AC Voltage Phase CN"       ))
-    allFields.add(device.wantField("Model 101", "AC Power"                  ))
-    allFields.add(device.wantField("Model 101", "AC Line Frequency"         ))
-    allFields.add(device.wantField("Model 101", "AC Apparent Power"         ))
-    allFields.add(device.wantField("Model 101", "AC Reactive Power"         ))
-    allFields.add(device.wantField("Model 101", "AC Power Factor"           ))
-    allFields.add(device.wantField("Model 101", "AC Energy"                 ))
-    allFields.add(device.wantField("Model 101", "DC Current"                ))
-    allFields.add(device.wantField("Model 101", "DC Voltage"                ))
-    allFields.add(device.wantField("Model 101", "DC Power"                  ))
-    allFields.add(device.wantField("Model 101", "Cabinet Temperature"       ))
-    allFields.add(device.wantField("Model 101", "Heat Sink Temperature"     ))
-    allFields.add(device.wantField("Model 101", "Transformer Temperature"   ))
-    allFields.add(device.wantField("Model 101", "Other Temperature"         ))
-
-    allFields.add(device.wantField("Model 160", "Module_0_Input ID"         ))
-    allFields.add(device.wantField("Model 160", "Module_0_DC Current"       ))
-    allFields.add(device.wantField("Model 160", "Module_0_DC Voltage"       ))
-    allFields.add(device.wantField("Model 160", "Module_0_DC Power"         ))
-    allFields.add(device.wantField("Model 160", "Module_1_Input ID"         ))
-    allFields.add(device.wantField("Model 160", "Module_1_DC Current"       ))
-    allFields.add(device.wantField("Model 160", "Module_1_DC Voltage"       ))
-    allFields.add(device.wantField("Model 160", "Module_1_DC Power"         ))
     return allFields
 }
 
 // ===================================================================================================
 
+if (modbusHost == null) {
+    println("Step 1: Edit the script and set the correct values for modbusHost, modbusPort and modbusUnit")
+    exitProcess(0)
+}
 
 print("Modbus: Connecting...")
 val modbusMaster = ModbusTCPMaster(modbusHost, modbusPort)
@@ -138,12 +108,25 @@ ModbusDeviceJ2Mod(modbusMaster, modbusUnit). use { modbusDevice ->
     val sunSpec = SunspecDevice.generate(modbusDevice) ?: throw ModbusException("Unable to generate SunSpec device")
     sunSpec.connect(modbusDevice, 100)
 
-    // To get information about what your every field your device CAN actually provide: uncomment this next line
-//    showAllFieldsWithUsableValues(sunSpec)
+    // To get information about what your every field your device CAN actually provide
+    if (allTheFieldsIWant(sunSpec).size <= 4) {
+        println("Step 2: Edit the script to include the fields you want.")
+        println("Below is the list of fields your device currently supports.")
+        println("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
+
+        showAllFields(sunSpec)
+
+        println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        println("This huge list thet just flashed by is the list of all possible fields.")
+        println("Step 2: Edit the script to include the fields you want, you can copy and paste what was just printed.")
+
+        exitProcess(0)
+    }
 
     // If no broker is specified the output is sent to the console (useful for testing)
     if (mqttBrokerHost == null || mqttTopic == null) {
-        println("No database, outputting to console")
+        println("Step 3: Edit the script to set the MQTT information.")
+        println("No MQTT broker specified, outputting to console")
         runLoop(sunSpec, null, "console")
         return
     }
@@ -171,12 +154,12 @@ fun runLoop(device: SchemaDevice, mqttClient: MqttClient?, mqttTopic: String) {
     val allFields = allTheFieldsIWant(device)
 
     // We need all the field we want.
-    allFields.forEach { it.need() }
+    allFields.forEach { it.second.need() }
 
     println("Trying to get ${allFields.size} fields.")
     allFields.forEach { println(it) }
     device.update()
-    println("Found ${allFields.filter{ it.value != null }.size} fields to have a value.")
+    println("Found ${allFields.filter{ it.second.value != null }.size} fields to have a value.")
     println(device.toTable(onlyUseFullFields = true))
 
     // ----------------------------------------------------------------------------------------
@@ -213,14 +196,15 @@ fun runLoop(device: SchemaDevice, mqttClient: MqttClient?, mqttTopic: String) {
             result.put("timestampString", now)
 
             allFields.forEach {
-                val jsonFieldName = it.jsonFieldName()
-                when(it.returnType) {
-                    DOUBLE     -> result.put(jsonFieldName, it.doubleValue     ?: 0.0)
-                    LONG       -> result.put(jsonFieldName, it.longValue       ?: 0)
-                    STRING     -> result.put(jsonFieldName, it.stringValue     ?: "")
-                    STRINGLIST -> result.put(jsonFieldName, it.stringListValue ?: listOf<String>())
-                    BOOLEAN    -> result.put(jsonFieldName, it.booleanValue     ?: "")
-                    UNKNOWN    -> TODO()
+                val jsonFieldName = it.first
+                val field = it.second
+                when(field.returnType) {
+                    DOUBLE     -> result.put(jsonFieldName, field.doubleValue     ?: 0.0)
+                    LONG       -> result.put(jsonFieldName, field.longValue       ?: 0)
+                    STRING     -> result.put(jsonFieldName, field.stringValue     ?: "")
+                    STRINGLIST -> result.put(jsonFieldName, field.stringListValue ?: listOf<String>())
+                    BOOLEAN    -> result.put(jsonFieldName, field.booleanValue    ?: "")
+                    UNKNOWN    -> TODO("This shouldn't happen")
                 }
             }
 
@@ -255,15 +239,27 @@ fun runLoop(device: SchemaDevice, mqttClient: MqttClient?, mqttTopic: String) {
 
 fun Field.jsonFieldName() = "${this.block.id} ${this.id}".replace(Regex("[^a-zA-Z0-9_]"), "_")
 
-fun showAllFieldsWithUsableValues(schemaDevice: SchemaDevice) {
+fun showAllFields(schemaDevice: SchemaDevice) {
     schemaDevice.updateAll()
-    println("All possible fields that provide a useful value:\n${schemaDevice.toTable(false)}")
-    exitProcess(0)
+    schemaDevice.blocks.forEach { block ->
+        block.fields.forEach { field ->
+            if (!field.isSystem) {
+                println("""
+    // [${field.block.id}]: ${field.id}
+    // ${field.description}${if (field.unit.isBlank()) "" else "\n    // Unit: ${field.unit}"}
+    // Seen value: ${field.value ?: "No value available. Not implemented/unused feature?"}
+    allFields.add("${field.jsonFieldName()}" to device.wantField("${field.block.id}", "${field.id}"))
+
+"""
+                )
+            }
+        }
+    }
 }
 
 fun generateHomeAssistantConfig(
     device: SchemaDevice,
-    allFields: List<Field>,
+    allFields: MutableList<Pair<String, Field>>,
 ) {
     // Generate the config for Home Assistant
     // We first fetch all fields that have been asked for
@@ -281,7 +277,7 @@ fun generateHomeAssistantConfig(
     configFields.add(model)
     configFields.add(version)
     configFields.add(serialNumber)
-    configFields.addAll(allFields)
+    configFields.addAll(allFields.map { it.second })
     configFields.distinct()
 
     configFields.forEach { it.need() }
@@ -296,24 +292,25 @@ mqtt:
     """.trimIndent())
     allFields
         .forEach {
-            if (!it.isSystem && it.value != null) {
-                val jsonFieldName = it.jsonFieldName()
+            val jsonFieldName = it.first
+            val field = it.second
+            if (!field.isSystem && field.value != null) {
                 // Building a name that looks 'ok' in Home Assistant
-                var name = if (it.block.shortDescription.isNullOrBlank())
-                    it.block.id
+                var name = if (field.block.shortDescription.isNullOrBlank())
+                    field.block.id
                 else
-                    it.block.shortDescription
+                    field.block.shortDescription
                 name += " - "
-                if(it.id.contains("_")) {
-                    name += it.id
+                if(field.id.contains("_")) {
+                    name += field.id
                         // Make the name of fields in repeating blocks look better
                         .replace(Regex("_([0-9]+)_"), "[$1].")
                         .replace("_", ".")
-                    if (!it.id.endsWith(it.shortDescription)) {
-                        name += " - ${it.shortDescription}"
+                    if (!field.id.endsWith(field.shortDescription)) {
+                        name += " - ${field.shortDescription}"
                     }
                 } else {
-                    name += it.shortDescription
+                    name += field.shortDescription
                 }
 
                 println(
@@ -321,8 +318,8 @@ mqtt:
     - name: "$name"
       unique_id: "SunSpec-${manufacturer.stringValue}-${serialNumber.stringValue}-$jsonFieldName"
       state_topic: "$mqttTopic"
-      value_template: "{{ value_json.$jsonFieldName ${if (it.returnType == DOUBLE) "| round(4, default=0)" else "" }}}"${if(it.unit.isNotBlank()) """
-      unit_of_measurement: "${it.unit}"""" else ""}
+      value_template: "{{ value_json.$jsonFieldName ${if (field.returnType == DOUBLE) "| round(4, default=0)" else "" }}}"${if(field.unit.isNotBlank()) """
+      unit_of_measurement: "${field.unit}"""" else ""}
       icon: mdi:solar-panel
       device:
         name: "${if(homeAssistantDeviceName.isNullOrBlank()) "${manufacturer.stringValue} ${model.stringValue}" else homeAssistantDeviceName}"
